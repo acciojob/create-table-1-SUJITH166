c1d5fd6cddfc82f9626f7165920a7139ae70ac57
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-07-29 17:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const sampleTable=document.getElementById("sampleTable")
+  const newrow=document.createElement('tr');
+
+	for(let i=0;i<2;i++)
+		{
+			const newcol=document.createElement('td');
+					newcol.textContent=`New Cell${i+1}`;
+			newrow.appendChild(newcol)
+		}  
+	sampleTable.appendChild(newrow)
 }
